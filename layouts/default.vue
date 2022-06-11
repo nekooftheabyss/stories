be1026d59addbeab7f428faebe7f278ae8062c64
Neko Of The Abyss <psi@innocent.com>
2022-06-11 23:45:55 +0530

    reorder

--- layouts/default.vue
+++ layouts/default.vue
@@ -43,8 +43,9 @@
     <LayoutFooter />
   </div>
 </template>
+
 <style>
-@import url("https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap");
+
 .kurobg {
   background-image: url("/bg/light.webp");
   background-attachment: fixed;
@@ -62,6 +63,9 @@
   background-repeat: no-repeat;
   background-size: contain;
 }
+
+@import url("https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap");
+
 </style> 
 <script setup>
 const nuxtApp = useNuxtApp();
